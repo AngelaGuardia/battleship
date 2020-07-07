8b0f06135b61c536394cbe9cc5afa12523901477
Angela Guardia <angelaguardia20@gmail.com>
2020-07-06 20:09:14 -0700

    Add tests for class and ship type

---
+++ test/ship_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_it_has_a_type
+
+    assert_equal "Cruiser", @cruiser.type
+  end
+end
