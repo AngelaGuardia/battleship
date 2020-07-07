00c81dbcb1778dc1a6ed233bb2558b6668c2f244
Ryan Barnett <ryanbarnett883@gmail.com>
2020-07-07 13:53:57 -0600

    Check Ship type and length can be different
    Your tests for type and length are perfect. 
    
    The extra tests I added verify the type and length attributes are dynamic.

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -5,6 +5,7 @@ require './lib/ship'
 class ShipTest < Minitest::Test
   def setup
     @cruiser = Ship.new("Cruiser", 3)
+    @destroyer = Ship.new("Destroyer", 2)
   end
 
   def test_it_exists
@@ -15,7 +16,15 @@ class ShipTest < Minitest::Test
     assert_equal "Cruiser", @cruiser.type
   end
 
+  def test_it_can_have_a_different_type
+    assert_equal "Destroyer", @destoryer.type
+  end
+
   def test_it_has_a_length
     assert_equal 3, @cruiser.length
+  end
+
+  def test_it_can_have_a_different_length
+    assert_equal 2, @destroyer.length
   end
 end
