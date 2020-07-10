7ba8aa9d461a037eb7bbbfb9359190b5ab323ce2
Angela Guardia <angelaguardia20@gmail.com>
2020-07-10 12:42:30 -0700

    Add start and get_user_input methods

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,25 @@ class Game
   def print_main_menu
     puts "🚢Welcome to BATTLESHIP ⚓ 🏴‍☠️\nEnter p to play. Enter q to quit."
   end
+
+  def start
+    print_main_menu
+    user_input = get_user_input
+    if user_input == "p"
+      #create New turn
+      # Turn.start_turn
+    else
+      puts "Thanks for Playing!"
+    end
+  end
+
+  def get_user_input
+    user_input = gets.chomp.downcase
+    unless user_input == "q" || user_input == "p"
+      puts "Enter p to play. Enter q to quit."
+      get_user_input
+    end
+    user_input
+  end
+
 end
