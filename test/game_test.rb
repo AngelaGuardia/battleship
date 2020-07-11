262fd6986b9fbd31dfbc218db0f4200ee78d19de
Angela Guardia <angelaguardia20@gmail.com>
2020-07-10 17:17:47 -0700

    Update players to include board and add test for players

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,15 +8,17 @@ require "./lib/game"
 
 class GameTest < Minitest::Test
   def setup
+    @board1 = Board.new()
     @cruiser1 = Ship.new("Cruiser", 3)
     @submarine1 = Ship.new("Submarine", 2)
     @ships1 = [@cruiser1, @submarine1]
-    @player1 = Player.new("Ada Lovelace", @ships1)
+    @player1 = Player.new("Ada Lovelace", @ships1, @board1)
 
+    @board2 = Board.new()
     @cruiser2 = Ship.new("Cruiser", 3)
     @submarine2 = Ship.new("Submarine", 2)
     @ships2 = [@cruiser2, @submarine2]
-    @player2 = Player.new("Watson", @ships2, true)
+    @player2 = Player.new("Watson", @ships2, @board1, true)
 
     @game = Game.new(@player1, @player2)
   end
@@ -27,4 +29,11 @@ class GameTest < Minitest::Test
 
   #can't test_it_prints_out_main_menu
   #can't test_get_user_input
+
+  def test_it_has_players
+    assert_instance_of Player, @game.player1
+    assert_equal @player1, @game.player1
+    assert_instance_of Player, @game.player2
+    assert_equal @player2, @game.player2
+  end
 end
