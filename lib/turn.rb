ba38de5b05492d14d47ce45dc86ac9768a947ee0
RyanDBarnett <conner2468@gmail.com>
2020-07-11 15:56:24 -0600

    Add human shot method to turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,15 @@ class Turn
     @player2 = player2
   end
 
+  def human_shot
+    p "Enter the coordinate for your shot:"
+    input = gets.chomp
+    while (!@player1.board.valid_coordinate?(input))
+      p "Please enter a valid coordinate:"
+      input = gets.chomp
+    end
+  end
+
   def display_boards
     "=============COMPUTER BOARD=============\n" +
     @player2.board.render +
