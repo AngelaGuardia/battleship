898180cf75052512666d957ebb708c9212855bf5
RyanDBarnett <conner2468@gmail.com>
2020-07-11 16:01:42 -0600

    Add valid_coordinate? test and modify valid_coordinates? test

--- test/board_test.rb
+++ test/board_test.rb
@@ -83,11 +83,16 @@ class BoardTest < Minitest::Test
     assert_instance_of Cell, board_cell_values[0]
   end
 
+  def test_it_knows_if_a_coordinate_is_valid
+    assert_equal true, @board.valid_coordinate?("A3")
+    assert_equal false, @board.valid_coordinate?("Not Right")
+  end
+
   def test_it_knows_a_ship_placement_is_on_valid_coordinates
-    assert_equal false, @board.valid_coordinate?(["A3", "A4", "A5"])
-    assert_equal true, @board.valid_coordinate?(["A2", "A3", "A4"])
-    assert_equal false, @board.valid_coordinate?(["A5"])
-    assert_equal true, @board.valid_coordinate?(["A4"])
+    assert_equal false, @board.valid_coordinates?(["A3", "A4", "A5"])
+    assert_equal true, @board.valid_coordinates?(["A2", "A3", "A4"])
+    assert_equal false, @board.valid_coordinates?(["A5"])
+    assert_equal true, @board.valid_coordinates?(["A4"])
   end
 
   def test_it_knows_a_ship_placement_has_a_valid_length
@@ -250,7 +255,7 @@ class BoardTest < Minitest::Test
      "3"=>["A3", "B3", "C3", "D3"],
      "4"=>["A4", "B4", "C4", "D4"]
    }
-   
+
     assert_equal expected, @board.columns
   end
 end
