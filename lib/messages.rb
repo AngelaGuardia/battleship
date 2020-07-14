9435f9e8c07f0c59d640a3225aa06037aaaf30a6
RyanDBarnett <conner2468@gmail.com>
2020-07-14 16:48:48 -0600

    Add layout messages method

--- lib/messages.rb
+++ lib/messages.rb
@@ -10,4 +10,23 @@ class Messages
   def self.play_or_quit
     "Enter p to play. Enter q to quit."
   end
+
+  def self.layout ships
+    message = "\nI have laid out my ships on the grid.\nYou now need to lay out your 3 ships.\n"
+    ships.each_with_index do |ship, i|
+      last_index = i == ships.length - 1
+      if i == 0
+        message += "The"
+      elsif last_index
+        message += ", and the"
+      else
+        message += ", the"
+      end
+      message += " #{ship[:type]} is #{ship[:length]} units long"
+      if last_index
+        message += ".\n"
+      end
+    end
+    message
+  end
 end
