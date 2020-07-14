f79898a3c67796e60bbf330146aab660cb0ab292
RyanDBarnett <conner2468@gmail.com>
2020-07-14 16:10:56 -0600

    Add messages main menu test

---
+++ test/messages_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/messages'
+
+class CellTest < Minitest::Test
+  def test_main_menu_message
+    assert_equal Messages.main_menu, "\n🚢Welcome to BATTLESHIP ⚓ 🏴‍☠️\nEnter p to play. Enter q to quit."
+  end
+
+end
