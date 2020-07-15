f657fd03911a23b9cd266db83e9cc6bc794619b2
RyanDBarnett <conner2468@gmail.com>
2020-07-15 13:46:27 -0600

    Require all classes in runner

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,4 +1,10 @@
 require "./lib/game"
+require "./lib/board"
+require "./lib/ship"
+require "./lib/player"
+require "./lib/cell"
+require "./lib/turn"
+require './lib/messages'
 
 game = Game.new
 game.start
