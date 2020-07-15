d58aceec21f6e3b4d291be95982ad374bd1b4130
RyanDBarnett <conner2468@gmail.com>
2020-07-15 09:21:58 -0600

    Move thanks method

--- lib/messages.rb
+++ lib/messages.rb
@@ -3,12 +3,12 @@ class Messages
     "\n🚢Welcome to BATTLESHIP ⚓ 🏴‍☠️\nEnter p to play. Enter q to quit."
   end
 
-  def self.thanks
-    "Thanks for Playing!"
-  end
-
   def self.play_or_quit
     "Enter p to play. Enter q to quit."
+  end
+
+  def self.thanks
+    "Thanks for Playing!"
   end
 
   def self.coordinates_prompt(ship)
