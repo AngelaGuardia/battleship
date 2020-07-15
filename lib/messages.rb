a98341ac50e068c6b5c857f84a23d36f38e69a87
RyanDBarnett <conner2468@gmail.com>
2020-07-14 22:09:31 -0600

    Add conjunction helper method

--- lib/messages.rb
+++ lib/messages.rb
@@ -13,20 +13,27 @@ class Messages
 
   def self.layout ships
     message = "\nI have laid out my ships on the grid.\nYou now need to lay out your 3 ships.\n"
-    ships.each_with_index do |ship, i|
-      last_index = i == ships.length - 1
-      if i == 0
-        message += "The"
-      elsif last_index
-        message += ", and the"
-      else
-        message += ", the"
-      end
+    ships.each_with_index do |ship, curr_i|
+      message += Messages.conjunction_helper(ships, curr_i)
       message += " #{ship[:type]} is #{ship[:length]} units long"
-      if last_index
+      if is_last
         message += ".\n"
       end
     end
     message
+  end
+
+  def self.conjunction_helper(ships, curr_i)
+    last_i = ships.length - 1
+    is_first = curr_i == 0
+    is_last = curr_i == last_i
+    
+    if is_first
+      "The"
+    elsif is_last
+      ", and the"
+    else
+      ", the"
+    end
   end
 end
