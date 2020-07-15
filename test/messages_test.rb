fff41794cdba6ff0b8dc0abd4b50d5b3f24d2973
RyanDBarnett <conner2468@gmail.com>
2020-07-15 13:17:41 -0600

    Fix message test class name

--- test/messages_test.rb
+++ test/messages_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/messages'
 
-class CellTest < Minitest::Test
+class MessagesTest < Minitest::Test
   def test_main_menu_message
     expected = "\n🚢Welcome to BATTLESHIP ⚓ 🏴‍☠️\nEnter p to play. Enter q to quit."
 
