3c7be2f572a26c846a0e90518f78428b1ff9872f
RyanDBarnett <conner2468@gmail.com>
2020-07-15 14:11:58 -0600

    Remove game message tests

--- test/game_test.rb
+++ test/game_test.rb
@@ -45,25 +45,4 @@ class GameTest < Minitest::Test
     assert @game.board1.valid_placement?(@game.ships1[0], coordinates1)
     assert @game.board1.valid_placement?(@game.ships1[1], coordinates2)
   end
-
-  def test_it_prints_a_message_after_computer_ship_placement
-    expected = "\nI have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long.\n"
-
-    assert expected, @game.computer_message
-  end
-
-  def test_it_prints_prompt_to_enter_coordinates
-    assert_equal "Enter the squares for the Cruiser (3 spaces):\n>", @game.coordinates_prompt(@game.ships1[0])
-  end
-
-  #can't test get_human_coordinates method
-
-  def test_it_prints_REprompt_to_enter_valid_coordinates
-    assert_equal "Those are invalid coordinates. Please try again:\n>" , @game.coordinates_reprompt
-  end
-
-  def test_it_can_end_the_game
-    assert_equal "I won! I'm the AI ruler of the world!", @game.end_game(@game.players[1])
-    assert_equal "You won! Woot woot!", @game.end_game(@game.players[0])
-  end
 end
